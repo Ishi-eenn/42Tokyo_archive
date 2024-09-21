78c68255332178edf32f9daca83e0620a7514357
Ishi <syi378039@gmail.com>
2024-09-22 02:06:51 +0900

    CPP-Module07 ex01

---
+++ ex01/includes/iter.hpp
@@ -0,0 +1,15 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template <typename T, typename F>
+void iter(T *array, const size_t length, F func) {
+  if (!array)
+    return;
+
+  for (size_t i = 0; i < length; i++)
+    func(array[i]);
+}
+
+#endif
